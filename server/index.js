269cf14ee0e6d00c5c41ee4e41acee4b348f0b69
Leo Yassuda <leo.yassuda@gmail.com>
2020-08-17 00:04:06 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const yup = require("yup");
+const monk = require("monk");
+const {
+    nanoid
+} = require("nanoid");
+
+require("dotenv").config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get("urls");
+urls.createIndex({
+    slug: 1
+}, {
+    unique: true
+});
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "shortfy-url | Short Urls for your",
+//   });
+// });
+
+// app.get("/url/:id", (req, res) => {
+//   // TODO: get short url by id
+// });
+
+// TODO: Verify why need it to get url params correctly
+app.get("/favicon.ico", (req, res) => res.status(204));
+
+app.get("/:id", async (req, res, next) => {
+    const {
+        id: slug
+    } = req.params;
+
+    console.log(">>> params: ", req.params);
+
+    try {
+        const url = await urls.findOne({
+            slug
+        });
+
+        console.log(">>> slog:", slug);
+        console.log(">>> url:", url);
+
+        if (url) {
+            console.log(">>>>>>>>>>>>>> url:", url.url);
+            res.redirect(url.url);
+        }
+        res.redirect(`/?error=${slug} not found`);
+    } catch (error) {
+        console.log("<<<<<< error", error);
+        res.redirect(`/?error=Link not found`);
+    }
+});
+
+const schema = yup.object().shape({
+    slug: yup
+        .string()
+        .trim()
+        .matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+});
+
+app.post("/url", async (req, res, next) => {
+    let {
+        slug,
+        url
+    } = req.body;
+    try {
+        await schema.validate({
+            slug,
+            url,
+        });
+        if (!slug) {
+            slug = nanoid(5);
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error) {
+        if (error.message.startsWith("E11000")) {
+            error.message = "Slug in use. 🍔";
+        }
+        next(error);
+    }
+});
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
+    });
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
---
+++ server/public/app.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        url: '',
+        slug: '',
+        created: null
+    },
+    methods: {
+        createUrl() {
+            console.log(this.url, this.slug);
+        }
+    }
+})
