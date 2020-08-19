4ddd11fa573662c509221b83981dcc2e24efebf4
Leo Yassuda <leo.yassuda@gmail.com>
2020-08-19 17:09:17 -0300

    changed origin cors

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ const allowlist = ["http://localhost", "https://chiwawer.vercel.app"];
 
 app.use((req, res, next) => {
   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", allowlist);
+  res.header("Access-Control-Allow-Origin", "*");
   //Quais são os métodos que a conexão pode realizar na API
   res.header("Access-Control-Allow-Methods", "GET,POST");
   app.use(cors());
