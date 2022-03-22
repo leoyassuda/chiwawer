1c7529753413389ad6fd77602a01555b915fb68d
Leo Yassuda <leo.yassuda@gmail.com>
2022-03-22 17:47:50 -0300

    feat: 🎸 update tools version project

--- api/alias/[alias].js
+++ api/alias/[alias].js
@@ -1,78 +1,118 @@
 // Import Dependencies
 const pino = require('pino');
 const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true
-    }
-  }
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+        },
+    },
 });
-const url = require("url");
-const MongoClient = require("mongodb").MongoClient;
+const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
 
 // Create cached connection variable
 let cachedDb = null;
 
 async function connectToDatabase(uri) {
-  // If the database connection is cached,
-  // use it instead of creating a new connection
-  if (cachedDb) {
-    logger.info('Using cacheDb ...');
-    return cachedDb;
-  }
+    // If the database connection is cached,
+    // use it instead of creating a new connection
+    if (cachedDb) {
+        logger.info({
+            db: {
+                message: 'Using cacheDb',
+                location: 'api/alias/[alias].js',
+                method: 'connectToDatabase',
+                data: cachedDb,
+            },
+            event: {
+                type: 'request',
+                tag: 'db',
+            },
+        });
+        return cachedDb;
+    }
 
-  // If no connection is cached, create a new one
-  const client = await MongoClient.connect(uri,
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
+    // If no connection is cached, create a new one
+    const client = await MongoClient.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
     });
 
-  const db = await client.db(url.parse(uri).pathname.substr(1));
+    // const db = await client.db(uri);
+    const db = await client.db(url.parse(uri).pathname.substr(1));
 
-  // Cache the database connection and return the connection
-  cachedDb = db;
-  return db;
+    // Cache the database connection and return the connection
+    cachedDb = db;
+    return db;
 }
 
-module.exports = async (req, res) => {
-  const alias = req.query.alias;
-  const query = {
-    alias: alias,
-  };
+export default async function handler(req, res) {
+    const { body } = req;
 
-  logger.info('Requesing alias redirect', query);
+    const alias = req.query.alias;
+    const query = {
+        alias: alias,
+    };
 
-  const db = await connectToDatabase(process.env.MONGO_URI);
-
-  const collection = await db.collection("urls");
-
-  const url = await collection.findOne(query);
-
-  if (url) {
     logger.info({
-      db: {
-        message: "Find one by alias",
-        location: "api/alias/[alias].js",
-        method: "findOne",
-        query: query,
-        data: url,
-      },
-      event: {
-        type: "request",
-        tag: "db",
-      },
+        db: {
+            message: 'Requesing alias redirect',
+            location: 'api/alias/[alias].js',
+            method: 'req.redirect',
+            data: query,
+        },
+        event: {
+            type: 'request',
+            tag: 'app',
+        },
     });
 
-    res.setHeader("Cache-Control", "s-maxage=3600");
-    return res.status(200).redirect(url.url);
-  }
+    const db = await connectToDatabase(process.env.MONGO_URI);
 
-  const proto = req.headers['x-forwarded-proto'] + req.headers['x-forwarded-port'];
-  const host = req.headers['x-vercel-deployment-url'] + '/#/aliasNotFound?aliasError=' + query.alias;
+    const collection = await db.collection('urls');
 
-  res.setHeader("Cache-Control", "s-maxage=3600");
-  res.status(200).redirect(proto + host);
+    const urlResult = await collection.findOne(query);
 
-};
+    if (urlResult) {
+        logger.info({
+            db: {
+                message: 'Find one by alias',
+                location: 'api/alias/[alias].js',
+                method: 'findOne',
+                query: query,
+                data: urlResult,
+            },
+            event: {
+                type: 'request',
+                tag: 'db',
+            },
+        });
+
+        res.setHeader('Cache-Control', 's-maxage=3600');
+        return res.status(200).redirect(urlResult.url);
+    } else {
+        logger.info({
+            db: {
+                message: 'cache',
+                location: 'api/alias/[alias].js',
+                method: 'findOne',
+                data: urlResult,
+            },
+            event: {
+                type: 'request',
+                tag: 'db',
+            },
+        });
+    }
+
+    const proto =
+        req.headers['x-forwarded-proto'] + req.headers['x-forwarded-port'];
+    const host =
+        req.headers['x-vercel-deployment-url'] +
+        '/#/aliasNotFound?aliasError=' +
+        query.alias;
+
+    res.setHeader('Cache-Control', 's-maxage=3600');
+    res.status(200).redirect(proto + host);
+}
